1b94e28a5ce053524d430902413fe661eb3ad4da
Julie <juliy0214561@gmail.com>
2024-07-26 21:30:26 +0800

    更改

--- vite.config.js
+++ vite.config.js
@@ -25,7 +25,7 @@ function moveOutputPlugin() {
 export default defineConfig({
   // base 的寫法：
   // base: '/Repository 的名稱/'
-  base: '/2024_hexschool_hw4/',
+  base: '/2024hexschool_week1_hw/',
   plugins: [
     liveReload(['./layout/**/*.ejs', './pages/**/*.ejs', './pages/**/*.html']),
     ViteEjsPlugin(),
